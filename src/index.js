dab6fcc559b00015ec2e9e848cb61d929f8d85f9
eternityseeker <eternityseeker@gmail.com>
2020-12-09 21:30:45 +0200

    learning

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
-  <div>
-    <h1>Hello</h1>
+  <div className="heading" contentEditable="false" spellCheck="false">
+    <h1 style={{ color : "blue" }} >Hello</h1>
+    <img src={img + "?grayscale"} alt="new pic" />
   </div>,
   document.getElementById("root")
 );
@@ -14,19 +17,31 @@ const fname = "moo";
 const lname = "nasr";
 const dt = new Date();
 
+const customeStyle = {
+color : "orange",
+fontSize : "20px",
+border : "1px solid black"
+};
+
+customeStyle.color = "violet";
+
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
+    <h1 style={customeStyle}>Hello {name}</h1>
     <h1>Hello {fname + " " + lname}</h1>
-    <h1>Hello {fname} {lname}</h1>
+    <h1>
+      Hello {fname} {lname}
+    </h1>
     <h1>Hello {`${fname} ${lname} 2`}</h1>
     <p>fav number is {num}</p>
     <p>fav number is {Math.floor(Math.random() * 10)}</p>
-    <p>fav number is {2+4}</p>
-<p> created by {fname} {lname} </p>
-<p>Copyright {dt.getFullYear()}</p>
+    <p>fav number is {2 + 4}</p>
+    <p>
+      {" "}
+      created by {fname} {lname}{" "}
+    </p>
+    <p>Copyright {dt.getFullYear()}</p>
   </div>,
   document.getElementById("root2")
 );
 console.log(dt);
-
