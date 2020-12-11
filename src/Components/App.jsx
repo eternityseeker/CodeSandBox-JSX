fc61431bc44d252e744dbecf3b3c7e1cdbc0fa1b
eternityseeker <eternityseeker@gmail.com>
2020-12-11 07:31:47 +0200

    update

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Heading from "./Heading";
 import List from "./List";
+//import PIPIPI, {pi2,pi3} from "../Extra/math"
+import * as PI from "../Extra/math"
+import * as Calculator from "../Extra/calculator"
 
 const num = 6;
 const fname = "moo";
@@ -9,6 +12,10 @@ const dt = new Date();
 
 function App() {
 return <div>
+  <table border="1" >
+  <tbody>
+<tr>
+  <td>
    <Heading />
     <List></List>
     <h1>Hello {fname + " " + lname}</h1>
@@ -18,12 +25,24 @@ return <div>
     <h1>Hello {`${fname} ${lname} 2`}</h1>
     <p>fav number is {num}</p>
     <p>fav number is {Math.floor(Math.random() * 10)}</p>
-    <p>fav number is {2 + 4}</p>
+    <p>fav number is {2 + 4} or {PI.default} or {PI.pi2}</p>
     <p>
       {" "}
       created by {fname} {lname}{" "}
     </p>
     <p>Copyright {dt.getFullYear()}</p>
+    </td>
+    <td>
+<ul>
+  <li>{Calculator.add(1,2)}</li>
+  <li>{Calculator.multiply(2,3)}</li>
+  <li>{Calculator.subtract(5,2)}</li>
+  <li>{Calculator.divide(9,3)}</li>
+</ul>
+      </td>
+      </tr>
+      </tbody>
+    </table>
     </div>
 ;
 }
---
+++ src/Extra/math.js
@@ -0,0 +1,6 @@
+const pi = 3.14;
+var pi2 = 3.14;
+var pi3 = 3.14;
+
+export default pi;
+export {pi2,pi3};
